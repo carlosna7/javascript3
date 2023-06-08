18b50bbfaf09ee62e0150e39170d0bbeca881c01
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-06-08 09:07:36 -0300

    att

--- content/16_parâmetros.js
+++ content/16_parâmetros.js
@@ -1,11 +1,53 @@
 // se não for passado nenhum valor no CONSOLE.LOG o valor a ser considera sera 1 atribuiudo como parametro na função multiplicar
 
-function multiplicar(a = 1, b = 1) {
-    return a * b;
-}
+// function multiplicar(a = 1, b = 1) {
+//     return a * b;
+// }
 
-console.log(multiplicar(5));
-console.log(multiplicar(5,5));
-console.log(multiplicar(undefined,6));
+// console.log(multiplicar(5));
+// console.log(multiplicar(5,5));
+// console.log(multiplicar(undefined,6));
 
 //---------------------------------------------
+
+// function test (num = 1) {
+//     console.log(typeof num);
+// }
+
+// test();
+// test(undefined);
+// test("");
+
+//---------------------------------------------
+
+// function concat(value, array = []) {
+//     array.push(value);
+//         return array;
+// }
+
+// concat(1);
+// concat(2);
+// console.log(concat(3));
+
+//---------------------------------------------
+
+// function chamaAlgo(coisa = algo()) {
+//     return coisa;
+// }
+
+// function algo() {
+//     return "Andefained";
+// }
+
+// const result = chamaAlgo();
+
+// console.log(result)
+
+//---------------------------------------------
+
+function somaArrayObjetos([x, y] = [1, 2], {z: z} = {z: 3}) {
+    return x + y + z;
+}
+
+console.log(somaArrayObjetos());
+console.log(somaArrayObjetos([3, 5], {z: 2}));
